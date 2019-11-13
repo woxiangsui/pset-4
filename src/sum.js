f86c7a2bbcaf54448a8645d21adcaeabaa425029
Jessica Lin <JLin@ucvts.tec.nj.us>
2019-11-13 12:58:42 -0500

    problem 1

--- src/sum.js
+++ src/sum.js
@@ -1,19 +1,20 @@
 const readlineSync = require("readline-sync");
 
+let lowboundint = Number(readlineSync.question("\nLower bound: "));
+let upboundint = Number(readlineSync.question("Upper bound: "));
+
 const MAX = Number.MAX_SAFE_INTEGER;
 const MIN = Number.MIN_SAFE_INTEGER;
-let lowboundint = 0;
-let upboundint = 0;
-let sum = lowboundint;
-let value = lowboundint;
+
+let sum = 0;
+let value = 0;
+
 do {
-    lowboundint = readlineSync.question("Lower bound: ");
-    upboundint = readlineSync.question("Upper bound: ");
-} while (lowboundint > upboundint);
+    lowboundint = Number(readlineSync.question("Lower bound: "));
+    upboundint = Number(readlineSync.question("Upper bound: "));
+} while (lowboundint > upboundint)
 
-while (sum <= upboundint) {
-    value = value++;
-    sum = value + sum;
+for (let value = 0; value <= upboundint; value++) {
+  sum = sum + value;
+  console.log(sum);
 }
-
-console.log(sum);
