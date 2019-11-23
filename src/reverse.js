525c563f6e1f513603600b66f7f2ba9544e71b2e
unknown <jlin@ucvts.org>
2019-11-23 18:48:54 -0500

    exercise 2

--- src/reverse.js
+++ src/reverse.js
@@ -2,15 +2,19 @@ const readlineSync = require("readline-sync");
 
 const MAX = Number.MAX_SAFE_INTEGER;
 const MIN = 1;
+let number;
+let digit = "";
 
-let integer = readlineSync.question("\nPositive integer: ");
+let integer = readlineSync.question("\n\nPositive integer: ");
 
-if (Number.isNaN(integer) || Number.isInteger(integer) || integer <= 0 && integer < MIN || integer > MAX) {
-  do {
+while (integer > MAX || integer < MIN || Number.isNaN(integer) || Number.isInteger(integer)) {
     integer = readlineSync.question("Positive integer: ");
-  } while (integer <= 0 && integer < MIN || integer > MAX ||Number.isNaN(integer) || Number.isInteger(integer));
-
-    
-} else {
-  console.log("No.");
-}
+  } while (integer > 0) {
+        let number = integer % 10;
+        integer = Math.floor(integer / 10);
+        if (integer >= 1) {
+          digit += number + ", ";
+        } else {
+          digit += number + ".";
+        }
+      } console.log("\n" + digit);
