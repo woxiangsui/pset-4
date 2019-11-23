8ddc32bbe1f6340af84b939aeceacf4d46a65bc4
unknown <jlin@ucvts.org>
2019-11-23 17:58:28 -0500

    input problem fixed

--- src/sum.js
+++ src/sum.js
@@ -8,17 +8,21 @@ const MIN = Number.MIN_SAFE_INTEGER;
 let sum = 0;
 let i = lowboundint;
 
-if (lowboundint > upboundint || lowboundint < MIN || upboundint > MAX || Number.isNaN(lowboundint) || Number.isNaN(upboundint)) {
+if (lowboundint > upboundint || lowboundint < MIN || upboundint > MAX ||
+    Number.isNaN(lowboundint) || Number.isNaN(upboundint) ||
+    !Number.isInteger(lowboundint) || !Number.isInteger(upboundint)) {
   do {
     lowboundint = Number(readlineSync.question("Lower bound: "));
     upboundint = Number(readlineSync.question("Upper bound: "));
-  } while (lowboundint > upboundint ||  lowboundint < MIN || upboundint > MAX || Number.isNaN(lowboundint) || Number.isNaN(upboundint));
+  } while (lowboundint > upboundint ||  lowboundint < MIN || upboundint > MAX ||
+     Number.isNaN(lowboundint) || Number.isNaN(upboundint) ||
+     !Number.isInteger(lowboundint) || !Number.isInteger(upboundint));
     if (i % 2 != +0) {
       lowboundint = lowboundint + 1;
     }
       for (let i = lowboundint; i <= upboundint; i = i + 2) {
         sum = sum + i;
-      } sum = sum.toLocaleString("en-US");
+      } sum = sum.toLocaleString("en");
         console.log("\n" + sum + ".");
 } else {
   if (i % 2 != +0) {
@@ -26,6 +30,6 @@ if (lowboundint > upboundint || lowboundint < MIN || upboundint > MAX || Number.
   }
   for (let i = lowboundint; i <= upboundint; i = i + 2) {
     sum = sum + i;
-  } sum = sum.toLocaleString("en-US");
+  } sum = sum.toLocaleString("en");
     console.log("\n" + sum + ".");
 }
