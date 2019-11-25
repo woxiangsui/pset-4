31598d77dd0aacdf1021cf50da60319a434dd651
unknown <jlin@ucvts.org>
2019-11-24 21:05:10 -0500

    exercise 4

--- src/average.js
+++ src/average.js
@@ -1,22 +1,30 @@
 const readlineSync = require("readline-sync");
 
-let integer = Number(readlineSync.question("\nNon-negative integer: "));
-
 const MIN = 0;
 const MAX = Number.MAX_SAFE_INTEGER;
-let number = 0;
+let count = 0;
 let sum = 0;
+let average = 0;
+let integer = 0;
+let isFalse = true;
+console.log();
 
-if (integer >= 0 && MIN <= integer <= MAX && !Number.isNaN(integer) && integer % 1 == 0) {
-  do {
-    integer = Number(readlineSync.question("Non-negative integer: "));
-    sum = sum + integer;
-    ++number;
-  } while (integer >= 0 && MIN <= integer <= MAX);
-} else if (Number.isNaN(integer) && integer % 1 != 0) {
-    --number;
+while (isFalse){
+  integer = Number(readlineSync.question("Non-negative integer: "));
+  if (integer >= MIN && integer <= MAX ){
+    sum += integer;
+    count ++;
+
+  }
+  else if (integer<0){
+    isFalse = false;
+  }
+
 }
 
-average = sum / number;
-average = average.toLocaleString("en", {minimumFractionDigits: 3, maximumFractionDigits: 3});
+average = sum / count;
+average = average.toLocaleString("en", {minimumFractionDigits: 3, maximumFractionDigits: 3 });
+
+
+
 console.log("\n" + average + ".");
